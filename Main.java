96ec105c3c2c292f8b6fa12d1e7144f04de75dc3
hkrishg <109535284+hkrishg@users.noreply.github.com>
2022-10-25 18:22:40 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,52 @@
+import java.util.*;
+
+class TreeNode {
+  int val;
+  TreeNode left;
+  TreeNode right;
+
+  TreeNode(int val) {
+    this.val = val;
+  }
+};
+
+class Main {
+
+//   Problem Statement #
+// Given a binary tree, populate an array to represent its level-by-level traversal in reverse order, i.e., the lowest level comes first. You should populate the values of all nodes in each level from left to right in separate sub-arrays.
+
+
+
+  private static List<List<Integer>> leverOrderTraversal(TreeNode root) {
+    List<List<Integer>> res = new ArrayList<>();
+    if (root == null)
+      return res;
+    Queue<TreeNode> queue = new LinkedList<>();
+    queue.add(root);
+    while (!queue.isEmpty()) {
+      List<Integer> currentLevel = new LinkedList<>();
+      int size = queue.size();
+      for (int i = 0; i < size; i++) {
+        TreeNode currentNode = queue.poll();
+        currentLevel.add(currentNode.val);
+        if (currentNode.left != null)
+          queue.add(currentNode.left);
+        if (currentNode.right != null)
+          queue.add(currentNode.right);
+      }
+      res.add(0, currentLevel);
+    }
+    return res;
+  }
+
+  public static void main(String[] args) {
+    TreeNode root = new TreeNode(12);
+    root.left = new TreeNode(7);
+    root.right = new TreeNode(1);
+        root.left.left = new TreeNode(9);
+    root.right.left = new TreeNode(10);
+        root.right.left = new TreeNode(5);
+
+    System.out.println(Main.leverOrderTraversal(root));
+  }
+}
